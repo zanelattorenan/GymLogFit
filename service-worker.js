11f990f91c59dc70c9583b647d4b2a04695dbd7b
zanelattorenan <zanelattorenan@gmail.com>
2025-11-25 18:23:04 -0300

    Add files via upload
    teste

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'gymlog-pro-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './js/core.js',
+  './js/treino.js',
+  './js/nutri.js',
+  './js/charts.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
