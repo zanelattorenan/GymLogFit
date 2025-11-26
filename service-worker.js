eec403dd4e5cc10dcd6000069cbd537f1c6affb6
zanelattorenan <zanelattorenan@gmail.com>
2025-11-26 16:02:37 -0300

    Update service-worker.js
    testete

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,30 @@
-const CACHE_NAME = 'gymlog-pro-inline-v1';
+const CACHE_NAME = 'gymlog-pro-v1';
 const URLS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  // Se quiser, pode voltar a colocar os JS aqui depois:
+  // './js/core.js',
+  // './js/treino.js',
+  // './js/nutri.js',
+  // './js/charts.js',
+  // './icons/icon-192.png',
+  // './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      // Em vez de cache.addAll (que quebra se 1 falhar),
+      // fazemos add() individual com catch.
+      return Promise.all(
+        URLS_TO_CACHE.map((url) =>
+          cache.add(url).catch((err) => {
+            console.warn('Falha ao adicionar ao cache:', url, err);
+          })
+        )
+      );
+    })
   );
 });
 
